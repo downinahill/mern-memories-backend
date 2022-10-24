274264b1ae487096b14f368d23e307997b71009c
downinahill <brian.hill.0023@gmail.com>
2022-10-23 23:47:05 -0400

    had to add NODE OPTIONS and --legacy-peer-deps as well as make the url http and not https

--- server/index.js
+++ server/index.js
@@ -12,10 +12,15 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
+var corsOptions = {
+  origin: 'http://localhost:3002',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+app.use('/posts', cors(corsOptions), postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://Brian:onthebus@cluster0.frcgs.mongodb.net/art?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 8000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
